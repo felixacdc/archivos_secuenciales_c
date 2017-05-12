895e724103b92fbc96544efcdf9c9f6a13c6bf90
felixacdc <pfmata01@gmail.com>
2017-05-12 11:30:39 -0600

    [add] crear fichero

--- main.cpp
+++ main.cpp
@@ -4,7 +4,31 @@
 
 using namespace std;
 
+struct {
+	int matricula;
+	char nombre[30];
+	int edad;
+}alumno;
+
 int main() {
-	
+	//creando y cerrando el archivo en disco
+	FILE *archdisco;
+	archdisco = fopen("alumnos.dat","w");
+	fclose(archdisco);
+	printf("ARCHIVO CREADO");
 	return 0;
 }
+
+/*
+“r” ? Lectura.
+
+“w” ? Escritura.
+
+“a” ? Append, si el archivo ya existe append empieza a añadir los nuevos datos al final del archivo ya existente.
+
+“r+” ? Lectura y escritura, ya debe existir el archivo.
+
+“w+” ? Crea para lectura y escritura y si ya existe, sobreescribe.
+
+“a+” ? Crea o abre para lectura y append, sino existe el archivo sera creado. 
+*/
